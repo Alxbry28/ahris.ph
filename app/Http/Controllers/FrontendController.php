a353143faa972f17aadf85221528b4210c20673e
UnityDevelopers PH <elitecyberdevelopersph@gmail.com>
2020-10-01 22:01:35 +0800

    Latest Repo and Update Pages

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    // base for Login
+    public function login() {
+    	return view('login');
+    }
+
+    //base for Register
+
+    public function register() {
+    	return view('register');
+    }
+
+    /// base for Blogs
+    public function ahrisblog() {
+        return view('blogs');
+    }
+
+    //ar-admin dashboard
+     public function adminDashboard() {
+    	return view('admin-dashboard.home');
+    }
+
+    //ar-admin Profile
+    public function adminProfile() {
+    	return view('admin-dashboard.ar-admin-profile');
+    }
+
+    //ar-analytics
+    public function adminAnalytics() {
+    	return view('admin-dashboard.ar-analytics');
+    }
+
+    //ar-crashlogs Report
+    public function adminCrashLogs() {
+    	return view('admin-dashboard.ar-crashlogs-report');
+    }
+
+    //ar-transaction Overview
+    public function adminOverview() {
+    	return view('admin-dashboard.ar-transaction-overview');
+    }
+
+    //ar-user Authentication
+    public function adminUserAuth() {
+    	return view('admin-dashboard.ar-user-authentication');
+    }
+
+    //ar-user User Control
+    public function adminUserControl() {
+    	return view('admin-dashboard.ar-user-control');
+    }
+
+    //ar-web Monitoring
+    public function adminMonitoring() {
+    	return view('admin-dashboard.ar-web-monitoring');
+    }
+
+    // ar-web Updates
+    public function adminWebUpdates() {
+    	return view('admin-dashboard.ar-web-updates');
+    }
+
+    // FOR USERS ROUTING FUNCTION
+
+    public function userProfile() {
+        return view('dashboard.profile');
+    }
+
+    public function userPersonalSettings() {
+        return view('dashboard.personal-settings');
+    }
+
+    public function modalUsername() {
+        return view('dashboard.modal');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('login', 'App\Http\Controllers\FrontendController@login');
+Route::get('register', 'App\Http\Controllers\FrontendController@register');
+Route::get('blogs', 'App\Http\Controllers\FrontendController@ahrisblog');
+// Admin Routes 
+Route::get('admin-dashboard/home', 'App\Http\Controllers\FrontendController@adminDashboard');
+Route::get('admin-dashboard/ar-admin-profile', 'App\Http\Controllers\FrontendController@adminProfile');
+Route::get('admin-dashboard/ar-analytics', 'App\Http\Controllers\FrontendController@adminAnalytics');
+Route::get('admin-dashboard/ar-crashlogs-report', 'App\Http\Controllers\FrontendController@adminCrashLogs');
+Route::get('admin-dashboard/ar-transaction-overview', 'App\Http\Controllers\FrontendController@adminOverview');
+Route::get('admin-dashboard/ar-user-authentication', 'App\Http\Controllers\FrontendController@adminUserAuth');
+Route::get('admin-dashboard/ar-user-control', 'App\Http\Controllers\FrontendController@adminUserControl');
+Route::get('admin-dashboard/ar-web-monitoring', 'App\Http\Controllers\FrontendController@adminMonitoring');
+Route::get('admin-dashboard/ar-web-updates', 'App\Http\Controllers\FrontendController@adminWebUpdates');
+
+Route::get('dashboard/profile', 'App\Http\Controllers\FrontendController@userProfile');
+Route::get('dashboard/personal-settings', 'App\Http\Controllers\FrontendController@userPersonalSettings');
+Route::get('dashboard/modal', 'App\Http\Controllers\FrontendController@modalUsername');
+
+
+
